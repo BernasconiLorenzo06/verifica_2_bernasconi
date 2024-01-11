576f02ab4561c65693bff71eb0cee59275d987af
BernasconiLorenzo06 <bernasconi.lorenzo@iisgalvanimi.edu.it>
2024-01-11 13:32:50 +0000

    punto 2

--- bernasconi/src/app/container/container.component.ts
+++ bernasconi/src/app/container/container.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.css']
 })
-export class ContainerComponent {
+export class ContainerComponent implements OnInit{
+  macchine : string[];
+  constructor() {
+    this.macchine = ["Tesla","Audi","Fiat","Ford","Jeep","Ferrari","Lamborghini","Porche","500","KIA"];
+  }
+  ngOnInit(): void {
+   
+  }
 
 }
